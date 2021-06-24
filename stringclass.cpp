7010b626c943aeb099a7012310d3cc3e1066fb59
Nishtha Srivastava <51073931+srivastavanishtha@users.noreply.github.com>
2021-06-24 15:46:41 +0530

    Update stringclass.cpp

--- stringclass.cpp
+++ stringclass.cpp
@@ -7,6 +7,11 @@ int main(){
     int x,y,z;
     std::cin>>x>>y>>z; //using commas doesn't generate an error but it only inputs the first one
     std::cout<<x<<y<<z<<std::endl; //using commas dosen't generate error but it only prints the first variable
+    /*also cin takes only the first word in a string because it considers spaces as termination, 
+    even tabs and newline s owe use getline()to get the entire thing*/
+    std::string str,str1;
+    getline(std::cin, str);
+    std::cin>>str1;
     std::cout<<a << std::endl;
     std::cout<<(10>5)<< std::endl;
     std::cout<<"The size of the string is "<< s.length()<<std::endl;
